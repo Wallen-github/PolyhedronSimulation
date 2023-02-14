9c01b82ccfb0c5cecde401190058b5fdda1cfcf4
hai <Hai-Shuo.Wang@colorado.edu>
2023-02-14 00:13:14 -0800

    version20230212

---
+++ version20230212/Computation.py
@@ -0,0 +1,149 @@
+# coding=utf-8
+
+"""
+@author: Hai-Shuo Wang
+@email: wallen1732@gamil.com
+@file: Computation.py
+@date: 1/10/23 13:50
+@desc: 
+"""
+
+from __future__ import print_function
+import os, sys
+from pylmgc90 import chipy
+from pykdgrav import *
+
+sys.path.append('..')
+
+chipy.Initialize()
+
+chipy.checkDirectories()
+
+chipy.utilities_DisableLogMes()
+# timer gravity
+timer_id = chipy.timer_GetNewTimer('gravity computation')
+
+chipy.SetDimension(3)
+
+dt = 5.e-5
+theta = 0.5
+nb_steps = 50000
+
+echo = 0
+
+freq_display = 400
+ref_radius = 5.
+freq_write = 400
+
+# freq_update = 600
+
+chipy.nlgs_3D_DiagonalResolution()
+chipy.RBDY3_NewRotationScheme()
+
+chipy.PRPRx_UseCpCundallDetection(300)
+chipy.PRPRx_LowSizeArrayPolyr(20)
+
+# type = 'Stored_Delassus_Loops         '
+stype = 'Exchange_Local_Global         '
+norm = 'QM/16'
+tol = 0.1e-3
+relax = 1.0
+gs_it1 = 10
+gs_it2 = 200
+
+chipy.utilities_logMes('INIT TIME STEPPING')
+chipy.TimeEvolution_SetTimeStep(dt)
+chipy.Integrator_InitTheta(theta)
+
+chipy.utilities_logMes('READ BODIES')
+chipy.ReadBodies()
+chipy.LoadTactors()
+
+chipy.utilities_logMes('READ INI DOF')
+chipy.ReadIniDof()
+
+chipy.utilities_logMes('READ BEHAVIOURS')
+chipy.ReadBehaviours()
+chipy.LoadBehaviours()
+
+chipy.utilities_logMes('READ INI Vloc Rloc')
+chipy.ReadIniVlocRloc()
+
+chipy.utilities_logMes('READ DRIVEN DOF')
+chipy.ReadDrivenDof()
+
+chipy.WriteBodies()
+#
+chipy.WriteBehaviours()
+
+chipy.WriteDrivenDof()
+
+chipy.ComputeMass()
+
+# chipy.RBDY3_FatalDamping()
+
+nbR3 = chipy.RBDY3_GetNbRBDY3()
+
+mass = np.zeros(nbR3)
+for i in range(nbR3):
+    mass[i] = chipy.RBDY3_GetMass(i + 1)
+
+coor = np.empty([nbR3, 6], dtype=float)
+p_coor = np.empty([nbR3, 3], dtype=float)
+vbeg = np.empty([nbR3, 6], dtype=float)
+
+fext = np.empty([nbR3, 6], dtype=float)
+cij = np.zeros(3)
+
+chipy.OpenDisplayFiles()
+chipy.WriteDisplayFiles(1)
+
+for k in range(1, nb_steps + 1):
+    print(k, '/', (nb_steps + 1))
+    #
+    chipy.IncrementStep()
+
+    chipy.ComputeFext()
+
+    fext = np.empty([nbR3, 6], dtype=float)
+
+    for i in range(0, nbR3, 1):
+        coor[i, :] = chipy.RBDY3_GetBodyVector('Coorb', i + 1)
+        p_coor[i, 0:3] = coor[i, 0:3]
+        fext[i, :] = 0.
+
+    chipy.timer_StartTimer(timer_id)
+    # fext[:, 0:3] = Accel(p_coor, mass, G=6.6742e-11)
+    fext[:, 0:3] = Accel(p_coor, mass, G=1)
+    for i in range(0, nbR3, 1):
+        fext[i, :] = fext[i, :] * mass[i]
+    chipy.timer_StopTimer(timer_id)
+
+    for i in range(0, nbR3, 1):
+        chipy.RBDY3_PutBodyVector('Fext_', i + 1, fext[i, :])
+
+    chipy.ComputeBulk()
+    chipy.ComputeFreeVelocity()
+
+    chipy.SelectProxTactors()
+    chipy.RecupRloc()
+
+    chipy.ExSolver(stype, norm, tol, relax, gs_it1, gs_it2)
+
+    chipy.StockRloc()
+
+    chipy.ComputeDof()
+    chipy.UpdateStep()
+
+    chipy.WriteDisplayFiles(freq_display, ref_radius)
+
+    chipy.WriteOutDof(freq_write)
+    chipy.WriteOutVlocRloc(freq_write)
+
+    chipy.overall_CleanWriteOutFlags()
+
+chipy.WriteLastDof()
+chipy.CloseDisplayFiles()
+
+chipy.Finalize()
+
